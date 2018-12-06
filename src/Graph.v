23cf4c4ae2e1ae324a6666dbdf7cf4aeb5e962ad
Alexandre Moine <alexandre@moine.me>
2018-12-06 18:07:55 +0100

    Other improvements

--- src/Graph.v
+++ src/Graph.v
@@ -1,4 +1,5 @@
 Require Import Coq.Program.Basics.
+Require Import Coq.Classes.RelationClasses.
 
 Inductive Graph A :=
   | Empty : Graph A
@@ -23,7 +24,7 @@ Definition bind {A B:Type} (f:A -> Graph B) (g:Graph A) := foldg Empty f Overlay
 
 Definition size {A:Type} (g : Graph A) := foldg 1 (fun _ => 1) (fun x y => x+y) (fun x y => x+y) g.
 
-Definition isEmpty {A:Type}  (g : Graph A) := foldg true (fun _ => false) orb orb g.
+Definition isEmpty {A:Type}  (g : Graph A) := foldg true (fun _ => false) andb andb g.
 
 Lemma inline_bind : forall A B (f_v : A -> Graph B),
   bind f_v = foldg Empty f_v Overlay Connect.
@@ -55,10 +56,13 @@ Require Import Coq.Classes.RelationClasses.
 *)
 
 Class EqG (A:Type) (R : relation (Graph A)) : Prop := {
-  (* Equiv *)
-  EqG_Reflexive :> Reflexive R ;
-  EqG_Symmetric :> Symmetric R ;
-  EqG_Transitive :> Transitive R ;
+  EqG_Equiv :> Equivalence R ;
+
+  (* Congruences *)
+  EqG_PlusLeftCong :> forall x y z, R x y -> R (Overlay x z) (Overlay y z);
+
+  EqG_TimesLeftCong :> forall x y z, R x y -> R (Connect x z) (Connect y z);
+  EqG_TimesRightCong :> forall x y z, R x y -> R (Connect z x) (Connect z y);
 
   (* Overlay *)
   EqG_PlusCommut :> forall x y, R (Overlay x y) (Overlay y x);
@@ -69,6 +73,18 @@ Class EqG (A:Type) (R : relation (Graph A)) : Prop := {
   EqG_TimesRightId :> forall x, R (Connect x Empty) x ;
   EqG_TimesAssoc :> forall x y z, R (Connect x (Connect y z)) (Connect (Connect x y) z);
  }.
+
+Require Import Coq.Setoids.Setoid.
+
+Lemma EqG_PlusRightCong (A:Type) (R: relation (Graph A)) : EqG A R ->
+  forall x y z, R x y -> R (Overlay z x) (Overlay z y).
+Proof.
+  intros H x y z r.
+  rewrite EqG_PlusCommut.
+  rewrite (EqG_PlusCommut z y).
+  apply EqG_PlusLeftCong.
+  exact r.
+Qed.
 
 Lemma eq_impl_eqG (A:Type) (R: relation (Graph A)) (a:Graph A) (b:Graph A): a=b -> EqG A R -> R a b.
 Proof.
@@ -81,3 +97,43 @@ Qed.
 Theorem id_Plus (A:Type) (R:relation (Graph A)) (x:Graph A) : EqG A R -> R (Overlay x Empty) x.
 Proof.
 Admitted.
+
+(* For Rewrite *)
+Require Import Setoid.
+Require Import Relation_Definitions.
+
+Add Parametric Morphism A (R: relation (Graph A)) (g:Graph A) `(EqG A R) : (Overlay g)
+  with signature (R) ==> R
+    as overlay_right_morph.
+Proof.
+  intros x y r.
+  apply (EqG_PlusRightCong A R H).
+  exact r.
+Qed.
+
+Add Parametric Morphism A (R: relation (Graph A)) (g:Graph A) `(EqG A R) : (fun x => Overlay x g)
+  with signature (R) ==> R
+    as overlay_left_morph.
+Proof.
+  intros x y r.
+  apply (EqG_PlusLeftCong x y g).
+  exact r.
+Qed.
+
+Add Parametric Morphism A (R: relation (Graph A)) (g:Graph A) `(EqG A R) : (Connect g)
+  with signature (R) ==> R
+    as connect_right_morph.
+Proof.
+  intros x y r.
+  apply (EqG_TimesRightCong x y g).
+  exact r.
+Qed.
+
+Add Parametric Morphism A (R: relation (Graph A)) (g:Graph A) `(EqG A R) : (fun x => Connect x g)
+  with signature (R) ==> R
+    as connect_left_morph.
+Proof.
+  intros x y r.
+  apply (EqG_TimesLeftCong x y g).
+  exact r.
+Qed.
--- src/SmartHomo.v
+++ src/SmartHomo.v
@@ -1,9 +1,11 @@
 Require Coq.Logic.FunctionalExtensionality.
+Require Import Coq.Program.Utils.
 Require Import Coq.Program.Basics.
 Require Import Coq.Arith.PeanoNat.
 Require Import Omega.
 
 Require Import Coq.Relations.Relation_Definitions.
+Require Import Coq.Classes.RelationClasses.
 
 Require Import Graph.
 Require Import ReducedHomo.
@@ -69,26 +71,71 @@ Qed.
 Lemma isEmpty_empty (A:Type) :isEmpty (Empty (A:=A)) = true.
 Proof. auto. Qed.
 
-Lemma ksimpl_c_left_empty_x (A : Type) (c : Graph A -> Graph A -> Graph A) (x:Graph A) :
-  kSimpl c Empty x = x.
+Require Import Coq.Bool.Bool.
+Require Import Setoid.
+
+
+Lemma ksimpl_c_left_empty_x (A : Type) (R: relation (Graph A)) (c : Graph A -> Graph A -> Graph A) (x:Graph A) :
+  EqG A R -> R (kSimpl c Empty x) x.
 Proof.
   unfold kSimpl.
   rewrite isEmpty_empty.
+  intros H.
+  induction x.
+  - simpl. reflexivity.
+  - simpl. reflexivity.
+  - unfold isEmpty.
+    simpl.
+    fold (isEmpty x1).
+    fold (isEmpty x2).
+    destruct (bool_dec (isEmpty x1) true).
+    destruct (bool_dec (isEmpty x2) true).
+   -- rewrite e. rewrite e0.
+      simpl.
+      rewrite e in IHx1. rewrite e0 in IHx2.
+      apply symmetry in IHx2.
+      rewrite IHx2.
+      rewrite (id_Plus A R x1 H).
+      exact IHx1.
+   -- apply not_true_is_false in n.
+      rewrite n.
+      rewrite andb_false_r.
+      reflexivity.
+   -- apply not_true_is_false in n.
+      rewrite n.
+      rewrite andb_false_l.
+      reflexivity.
+  - unfold isEmpty.
+    simpl.
+    fold (isEmpty x1).
+    fold (isEmpty x2).
+    destruct (bool_dec (isEmpty x1) true).
+    destruct (bool_dec (isEmpty x2) true).
+   -- rewrite e. rewrite e0.
+      simpl.
+      rewrite e in IHx1. rewrite e0 in IHx2.
+      apply symmetry in IHx2.
+      rewrite IHx2.
+      rewrite EqG_TimesRightId.
+      exact IHx1.
+   -- apply not_true_is_false in n.
+      rewrite n.
+      rewrite andb_false_r.
+      reflexivity.
+   -- apply not_true_is_false in n.
+      rewrite n.
+      rewrite andb_false_l.
+      reflexivity.
+Qed.
+(* 
+Lemma ksimpl_c_right_empty_x (A : Type) (R: relation (Graph A)) (c : Graph A -> Graph A -> Graph A) (x:Graph A) :
+  EqG A R -> R (kSimpl c x Empty) x.
 Admitted.
-(*
-Lemma ksimpl_c_right_empty_x (A : Type) (c : Graph A -> Graph A -> Graph A) (x:Graph A) :
-  kSimpl c x Empty = x.
-Admitted.
-
-Lemma ksimpl_not_empty (A : Type) (c : Graph A -> Graph A -> Graph A) (x y:Graph A) :
-  x <> Empty /\ y <> Empty -> kSimpl c x y = c x y.
-Admitted. *)
 
 Lemma graph_empty_or_not2 (A:Type) : forall (x y:Graph A), x=Empty \/ y=Empty \/ (x <> Empty /\ y <> Empty).
 Admitted.
 
-(* False lemma *)
-(* Theorem smart_hom_is_reduced_hom A B (R: relation (Graph B)) (f : Graph A -> Graph B) :
+Theorem smart_hom_is_reduced_hom A B (R: relation (Graph B)) (f : Graph A -> Graph B) :
   EqG B R -> Smart_hom f -> Reduced_hom R f.
 Proof.
   intros H S.
@@ -102,12 +149,12 @@ Proof.
    -- rewrite H0.
       rewrite EqG_PlusCommut.
       rewrite (id_Plus B R (f b) H).
-      rewrite ksimpl_c_left_empty_x.
+      rewrite (ksimpl_c_left_empty_x B R Connect (f b) H).
       reflexivity.
    -- destruct H0.
     --- rewrite H0.
         rewrite (id_Plus B R (f a) H).
-        rewrite ksimpl_c_right_empty_x.
+        rewrite (ksimpl_c_right_empty_x B R Overlay (f a) H).
         reflexivity.
     --- rewrite (ksimpl_not_empty B Overlay (f a) (f b) H0). reflexivity.
   - intros a b.
